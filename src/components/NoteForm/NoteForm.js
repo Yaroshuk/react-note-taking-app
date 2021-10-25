7702015e2f3f273e3e922a9a39c754201a79e30c
Sergey Yaroshuk <yaroshukmail@gmail.com>
2021-10-25 22:51:56 +0300

    Add: note form textarea styles

--- src/components/NoteForm/NoteForm.js
+++ src/components/NoteForm/NoteForm.js
@@ -1,12 +1,15 @@
 import Button from "../UI/Button";
+import FilterContainer from "../Filter/FilterContainer";
 
 import classes from "./NoteForm.module.scss";
+
+const TEXTAREA_PLACEHOLDER = "Note contetnt...";
 
 const NoteForm = () => {
   return (
     <div className={classes["note-form"]}>
       <form>
-        <textarea />
+        <textarea autoFocus placeholder={TEXTAREA_PLACEHOLDER} />
         <div className={classes["note-form-control"]}>
           <Button title="cancel" action="cancel" />
           <Button title="save" type="sybmit" />
