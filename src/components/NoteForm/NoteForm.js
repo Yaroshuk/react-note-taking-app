da4c2fded69500be4728fa9de06ad149b9ad7765
Sergey Yaroshuk <yaroshukmail@gmail.com>
2021-10-25 22:59:21 +0300

    Add: filter indicator to note form

--- src/components/NoteForm/NoteForm.js
+++ src/components/NoteForm/NoteForm.js
@@ -1,6 +1,9 @@
 import Button from "../UI/Button";
 import FilterContainer from "../Filter/FilterContainer";
 
+import { filterClasses } from "../Filter/FilterItem";
+import { COLOR_FILTER } from "../../store/constants";
+
 import classes from "./NoteForm.module.scss";
 
 const TEXTAREA_PLACEHOLDER = "Note contetnt...";
@@ -8,6 +11,7 @@ const TEXTAREA_PLACEHOLDER = "Note contetnt...";
 const NoteForm = () => {
   return (
     <div className={classes["note-form"]}>
+      <div className={`${classes["note-filter"]} ${filterClasses[COLOR_FILTER.none]}`}/>
       <form>
         <textarea autoFocus placeholder={TEXTAREA_PLACEHOLDER} />
         <div className={classes["note-form-control"]}>
